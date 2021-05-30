7d5fb371460702f8d7d213cdfb2788c70107c65e
AnirudhMaheshwaram <55203441+AnirudhMaheshwaram@users.noreply.github.com>
2021-05-30 00:02:05 -0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,87 +1,98 @@
-# need to follow exact input instructions at https://gist.github.com/rraval/2ef5e2ff228e022653db2055fc12ea9d
-# supports an arbitrary number of arguments
+#S-expression calculator
+# need to follow exact input syntax specified at https://gist.github.com/rraval/2ef5e2ff228e022653db2055fc12ea9d
+# Can accept multiple argument expression like (add 1 2 3 4 (multiply 2 3 5))
+
 import sys
-operators = ['add','multiply']
-digits = ['0','1','2','3','4','5','6','7','8','9']
-other_char = ['(',' ',')']
+#to ammend operator add here and define funtion with same name
+operators = ['add','multiply'] 
+parentheses = ['(', ')']
+#digits = ['0','1','2','3','4','5','6','7','8','9']
+
+#err handling
 def simp_err_hand(err_msg):
-    raise TypeError(err_msg) from None
-    
+    raise RuntimeError(err_msg) from None
+
+#converts input string into array of expression and parentheses
+# example: string '(add 123 456)' converted into array ['(','add','123','456',')']
+def str_conv(inp_str):
+    inp_str = inp_str.replace(')',' ) ')
+    inp_str = inp_str.replace('(',' ( ')
+    inp_str += ' '
+    exp_arr = []
+    substr = ''
+    for ch in inp_str:
+        if ch == ' ':
+            if len(substr)>0:
+                exp_arr+=[substr]
+            substr = ''
+        else: substr+=ch
+    return exp_arr
+
+#verifies if string is positive integer
+def isnumber(exp):
+    return exp.isnumeric()
+
+#checks if string is one of operators
+def isoperator(exp):
+    return exp in operators
+
+#checks if string is one of operators
+def isparentheses(exp):
+    return exp in parentheses
+
+#checks if string is a parentheses
+def check_exp_arr(exp_arr):
+    for exp in exp_arr:
+        if not( isnumber(exp) or isoperator(exp) or isparentheses(exp) ):
+            simp_err_hand("invalid expression syntax: "+ exp)
+
+#function for operator 'add'
 def add(num):
     if(len(num)<2):
-        simp_err_hand("low number of attributes")
+        simp_err_hand("low number of integer expressions")
     result = 0
     for n in num:
         result += n
     return result
-    
+
+#function for operator 'multiply'    
 def multiply(num):
     if(len(num)<2):
-        simp_err_hand("low number of attributes")
+        simp_err_hand("low number of integer expressions")
     result = 1
     for n in num:
         result = result*n
     return result
-
-def check_exp(inp_exp):
-    inp_exp = inp_exp.replace('\"','')
-    inp_exp = inp_exp.replace(')',') ')
-    val_exp = operators+digits+other_char
-    i = 0
-    counter = 0
-    while(i<len(inp_exp)):
-        if inp_exp[i]=='(': 
-            counter+=1
-        if inp_exp[i]==')':
-            counter-=1
-        
-        flag = 0
-        for exp in val_exp:
-            ln = len(exp) 
-            if(inp_exp[i:i+ln]==exp):
-                i = i+ln
-                flag = 1
-                break
-        if flag ==0:
-            simp_err_hand("invalid expression: "+inp_exp[i])
-        if counter<0:
-            simp_err_hand("invalid parentheses: missing '('")
-    if counter!=0:
-        simp_err_hand("invalid parentheses: missing ')'")
-    return inp_exp
-def solv_exp(inp_exp):
-    inp_exp += ' '
-    if inp_exp.replace(" ","").isnumeric():
-        return([int(inp_exp)],len(inp_exp))
-    itr = 0
+    
+#solves expression array 
+def solv_exp(exp_arr):
+    operator = ''
     numbers = []
-    operator = ""
+    itr = 0
     retlen = 0
-    while(itr<len(inp_exp)-1):
-        if(inp_exp[itr] == '('):
-            (num,ln) = solv_exp(inp_exp[itr+1:len(inp_exp)])
+    while itr<len(exp_arr):
+        exp = exp_arr[itr]
+        if exp == '(': # recurring function
+            (num,ln) = solv_exp(exp_arr[itr+1:len(exp_arr)])
             numbers += [num]
             retlen += ln
-            inp_exp = inp_exp[:itr]+inp_exp[itr+ln+1:]
-        if(inp_exp[itr] == ')'):
+            exp_arr = exp_arr[:itr]+exp_arr[itr+ln:]
+            itr -= 1
+        elif exp == ')':
+            itr+=1
             break
-        l=1
-        while(inp_exp[itr+l]!=' ' and inp_exp[itr+l]!=')' and inp_exp[itr+l]!='('):
-            l=l+1
-        substr = inp_exp[itr:itr+l].replace(" ","")
-        if substr.isnumeric():
-            numbers += [int(substr)]
-        elif substr in operators:
-            if len(operator)>0:
-                simp_err_hand("invalid expression syntax: "+ substr)
-            operator = substr
-            
-        itr+=l
-    result = eval(operator+'(numbers)') 
+        elif isnumber(exp):
+            numbers += [int(exp)]
+        elif isoperator(exp) and len(operator)==0:
+            operator = exp
+        else: simp_err_hand("invalid expression syntax: "+ exp)
+        itr+=1
+    result = eval(operator+'(numbers)')
     return(result,retlen+itr+1)
-      
+    
 
-inp_exp = sys.argv[1]
-inp_exp = check_exp(inp_exp)
-num,ln = solv_exp(inp_exp)
+inp_str = sys.argv[1]
+exp_arr = str_conv(inp_str)
+inp_exp = check_exp_arr(exp_arr)
+num,ln = solv_exp(exp_arr)
 print(num[0])
